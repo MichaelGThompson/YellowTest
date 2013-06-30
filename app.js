513cf1b3733d52dd5539454a74a953f62d09662e
Michael Thompson <mgt1964@gmail.com>
2013-06-30 20:42:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.VCAP_APP_PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
